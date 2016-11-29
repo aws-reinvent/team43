66518caeaf37c592d229b3bcb1e0f38d8975c420
John Kelvie <john.kelvie@gmail.com>
2016-11-28 17:39:33 -0800

    Added more stuff

--- src/main/java/com/team43/rest/FeedResource.java
+++ src/main/java/com/team43/rest/FeedResource.java
@@ -22,12 +22,6 @@ public class FeedResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public ESService.TweetArray getMap() {
-        ESService.TweetArray array = new ESService.TweetArray();
-        array.tweets = new ESService.Tweet[2];
-        array.tweets[0] = new ESService.Tweet();
-        array.tweets[0].text = "This is a tweet";
-        array.tweets[1] = new ESService.Tweet();
-        array.tweets[1].text = "This is also a tweet";
-        return array;
+        return new ESService().getTweets();
     }
 }
--- src/main/java/com/team43/service/ESService.java
+++ src/main/java/com/team43/service/ESService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Singleton;
+import java.util.Date;
 
 /**
  * Created by jpk on 11/28/16.
@@ -14,10 +15,29 @@ import javax.inject.Singleton;
 public class ESService {
     public TweetArray getTweets () {
         //String url = "https://search-hackathon43-hh6di4idan2fs35uldydhyhnyy.us-west-2.es.amazonaws.com/_search";
-        String url = "https://mmih9traja.execute-api.us-west-2.amazonaws.com/dev/query";
-        RestTemplate restTemplate = new RestTemplate();
-        TweetArray array = restTemplate.getForObject(url, TweetArray.class);
+//        String url = "https://mmih9traja.execute-api.us-west-2.amazonaws.com/dev/query";
+//        RestTemplate restTemplate = new RestTemplate();
+//        TweetArray array = restTemplate.getForObject(url, TweetArray.class);
+        ESService.TweetArray array = new ESService.TweetArray();
+        array.tweets = new ESService.Tweet[3];
+        array.tweets[0] = newTweet("This is a tweet");
+        array.tweets[1] = newTweet("This is a tweet also");
+        array.tweets[2] = newTweet("This is another tweet");
+
+        for (Tweet tweet : array.tweets) {
+            if (tweet.date != null) {
+                tweet.dateString = tweet.date.toString();
+            }
+        }
         return array;
+    }
+
+    public Tweet newTweet (String text) {
+        Tweet tweet = new ESService.Tweet();
+        tweet.text = text;
+        tweet.date = new Date();
+        tweet.username = "JPK";
+        return tweet;
     }
 
     @JsonIgnoreProperties(ignoreUnknown = true)
@@ -28,5 +48,8 @@ public class ESService {
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Tweet {
         public String text;
+        public String username;
+        public Date date;
+        public String dateString;
     }
 }
